71fa179aa27851d41021a0f5fdd86610c86d1666
Jeffrey Silverman <jeffreysilverman@gmail.com>
2016-09-22 11:18:40 -0700

    tried learning curves and made new maps

--- wheresious/get_scores.py
+++ wheresious/get_scores.py
@@ -1,6 +1,5 @@
 # initial imports
 import pandas as pd
-import os.path
 
 # fire up the US Census Geocode
 from censusgeocode import CensusGeocode
@@ -50,27 +49,14 @@ def one_score(address,naics_code_simple):
         # get census tract
         tract = float(address_info[0]['geographies']['Census Tracts'][0]['TRACT'])
 
-        # read-in business scores for all census tracts
-        filename = '/Users/jsilverman/insight/project/wheresious/static/tracts_scores_'+str(naics_code_simple)+'.csv'
-        if os.path.isfile(filename):        
-            tracts_scores = pd.read_csv(filename,sep=' ',names=['tracts','scores'])
-        else:
-            tracts_scores = pd.read_csv('/Users/jsilverman/insight/project/wheresious/static/tracts_scores_all.csv',sep=' ',names=['tracts','scores'])
+        # read in dataframe of tracts, codes, and scores
+        df_scores = pd.read_csv('/Users/jsilverman/insight/project/wheresious/static/tracts_codes_scores.csv',index_col=0)
 
-        # find score for input census tract (possibly use business code as well)
-        return float(tracts_scores[tracts_scores.tracts==tract].scores.to_string(index=False))
+        # get correct row of dataframe
+        score_row = df_scores[(df_scores.census_tract==tract) & (df_scores.code_simple==int(naics_code_simple))].score
+
+        # return actual score
+        return int(round(score_row.values[0]))
 
     except:
         return -1
-
-
-
-# get scores for all census tracts and return a heatmap of the scores
-# possibly using the NAICS code as well
-def all_scores(naics_code_simple):
-    # read-in business scores for all census tracts
-
-    # turn scores and tracts into a heat map (possibly use business code as well)
-    score_map = [1.]
-    
-    return score_map
--- wheresious/views.py
+++ wheresious/views.py
@@ -3,8 +3,8 @@ from flask import render_template
 from flask import request
 from wheresious import app
 from get_scores import one_score
-from get_scores import all_scores
-import os.path
+import numpy as np
+import pandas as pd
 
 
 
@@ -39,18 +39,17 @@ def single_lookup():
     address = request.args.get('address')
     naics_code_simple = request.args.get('naics_code_simple')
 
-    # get score from external file
+    # get score for this address and code
     score = one_score(address,naics_code_simple)
 
-    # see if NAICS code-specific scores exist
-    filename = '/Users/jsilverman/insight/project/wheresious/static/tracts_scores_'+str(naics_code_simple)+'.csv'
-    if os.path.isfile(filename):
-        code_used = True
-    else:
-        code_used = False
-            
+    # read in dataframe of tracts, codes, and scores
+    df_scores = pd.read_csv('/Users/jsilverman/insight/project/wheresious/static/tracts_codes_scores.csv',index_col=0)
+
+    # get average overall score for SD
+    mean_score = int(float(np.mean(df_scores.score)))
+    
     # render page
-    return render_template("one_address.html", address = address, naics_code_simple = naics_code_simple, score = score, code_used = code_used)
+    return render_template("one_address.html", address = address, naics_code_simple = naics_code_simple, score = score, mean = mean_score)
 
 
 
@@ -60,8 +59,54 @@ def full_city_lookup():
     # pull input field and store it
     naics_code_simple = request.args.get('naics_code_simple')
 
-    # get map of scores from external file
-    score_map = all_scores(naics_code_simple)
-    
+    if naics_code_simple=='11':
+        description = 'Agriculture, Forestry, Fishing and Hunting'
+    elif naics_code_simple=='22':
+        description = 'Utilities'
+    elif naics_code_simple=='23':
+        description = 'Construction'
+    elif naics_code_simple=='31':
+        description = 'Manufacturing'
+    elif naics_code_simple=='32':
+        description = 'Manufacturing'
+    elif naics_code_simple=='33':
+        description = 'Manufacturing'
+    elif naics_code_simple=='42':
+        description = 'Wholesale Trade'
+    elif naics_code_simple=='44':
+        description = 'Retail Trade'
+    elif naics_code_simple=='45':
+        description = 'Retail Trade'
+    elif naics_code_simple=='48':
+        description = 'Transportation and Warehousing'
+    elif naics_code_simple=='49':
+        description = 'Transportation and Warehousing'
+    elif naics_code_simple=='51':
+        description = 'Information'
+    elif naics_code_simple=='52':
+        description = 'Finance and Insurance'
+    elif naics_code_simple=='53':
+        description = 'Real Estate and Rental and Leasing'
+    elif naics_code_simple=='54':
+        description = 'Professional, Scientific, and Technical Services'
+    elif naics_code_simple=='55':
+        description = 'Management of Companies and Enterprises'
+    elif naics_code_simple=='56':
+        description = 'Administrative and Support and Waste Management and Remediation Services'
+    elif naics_code_simple=='61':
+        description = 'Educational Services'
+    elif naics_code_simple=='62':
+        description = 'Health Care and Social Assistance'
+    elif naics_code_simple=='71':
+        description = 'Arts, Entertainment, and Recreation'
+    elif naics_code_simple=='72':
+        description = 'Accommodation and Food Services'
+    elif naics_code_simple=='81':
+        description = 'Other Services (except Public Administration)'
+    elif naics_code_simple=='92':
+        description = 'Public Administration'
+    else:
+        description = 'Something went wrong with the NAICS code...'
+        
     # render page
-    return render_template("all_city.html", naics_code_simple = naics_code_simple, score_map = score_map)
+    return render_template("all_city.html", naics_code_simple = naics_code_simple, description = description)
